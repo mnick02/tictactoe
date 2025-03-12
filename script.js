611d8807c703c70368142c76cfe8cd35306fabcf
mnick02 <nokiamn3@gmail.com>
2025-03-12 12:21:16 -0700

    Create 2d array to rpresent state of game board

--- script.js
+++ script.js
@@ -5,4 +5,11 @@ function Gameboard() {
     const rows = 3;
     const columns = 3;
     const board = [];
+
+    for (let i = 0; i < rows; i++) {
+        board[i] = [];
+        for(let j = 0; j < columns; j++) {
+            board[i].push(Cell());
+        }
+    }
 }
