56c08842daca9349b51e5420ca8057c0489f9109
mnick02 <nokiamn3@gmail.com>
2025-03-12 12:09:47 -0700

    Create gameboard obj; rows, columns, and board array

---
+++ script.js
@@ -0,0 +1,8 @@
+//gameboard stored in array in gameboard obj
+//players stord in obj, obj to control game flow
+
+function Gameboard() {
+    const rows = 3;
+    const columns = 3;
+    const board = [];
+}
