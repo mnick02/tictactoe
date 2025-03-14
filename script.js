2ba02c4aa4f3aaffa178314bcba5551a15f9817c
mnick02 <nokiamn3@gmail.com>
2025-03-14 12:41:03 -0700

    Add checkWinner func
    Add checkWinner func; have it iterate through the
    board and compare vals with getValue and specific
    cases for diagonals, return true if it matches;
    gets called when a token is placed

--- script.js
+++ script.js
@@ -25,13 +25,57 @@ function Gameboard() {
         }
     };
 
+    const checkWinner = (player) => {
+        console.log("in Winner");
+        //console.log(`This is player: ${player}`);
+        
+        for (let i = 0; i < rows; i++) {
+            //console.log(`i is: ${i}`);
+            //console.log(board[i][0].getValue);
+            if (board[i][0].getValue() === player &&
+                board[i][1].getValue() === player &&
+                board[i][2].getValue() === player) {
+                
+                return true;
+            }
+        }
+
+                
+        for (let j = 0; j < columns; j++) {
+            if (board[0][j].getValue() === player &&
+                board[1][j].getValue() === player &&
+                board[2][j].getValue() === player) {
+                return true;
+            }
+        }
+
+        if (board[0][0].getValue() === player && 
+            board[1][1].getValue() === player && 
+            board[2][2].getValue() === player) {
+            return true;
+        }
+
+        if (board[2][0].getValue() === player && 
+        board[1][1].getValue() === player && 
+        board[0][2].getValue() === player) {
+        return true;
+    }
+
+        return false;
+    }
+    
+
     const printBoard = () => {
       const boardWithCellValues = board.map((row) => row.map((cell) => cell.getValue()))
       console.log(boardWithCellValues);
-      console.log("In printBoard");
+      //console.log("In printBoard");
+      //console.log("BORAD IS: ")
+      //console.log(checkScore(board));
+      //console.log("HELLLO");
     };
 
-    return {getBoard, placeItem, printBoard};
+
+    return {getBoard, placeItem, checkWinner, printBoard};
 
 }
 
@@ -75,7 +119,7 @@ function GameController (
 
     const printNewRound = () => {
         board.printBoard();
-        console.log("In printNewRound");
+        //console.log("In printNewRound");
         console.log(`${getActivePlayer().name}'s turn.`);
     };
 
@@ -84,8 +128,18 @@ function GameController (
             `Placing
             ${getActivePlayer().name}'s token on board`
         );
-        board.placeItem(column, rowIndex, getActivePlayer().token);
-        console.log(getActivePlayer().token);
+        const place = board.placeItem(column, rowIndex, getActivePlayer().token);
+        //console.log(getActivePlayer().token);
+
+        if (place) {
+            console.log("In place");
+            if (board.checkWinner(getActivePlayer().token)) {
+                board.printBoard();
+                console.log(`${getActivePlayer().name} wins!`)
+                return;
+            }
+            console.log(`${board.checkWinner(getActivePlayer.token)}: is value of winner`);
+        }
 
         switchPlayerTurn();
         printNewRound();
